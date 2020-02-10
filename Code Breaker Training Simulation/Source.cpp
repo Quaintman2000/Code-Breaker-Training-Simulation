0cd6603cd4a9092c10e21b94df63b9278a66a4c3
a <a>
2020-02-09 18:06:46 -0700

    Added No hint difficulty

--- Code Breaker Training Simulation/Source.cpp
+++ Code Breaker Training Simulation/Source.cpp
@@ -7,33 +7,70 @@ using namespace std;
 
 bool playAgain = true;
 bool wordCleared;
+string difficultyAnswer;
 string answer;
 string lastChoice[4] = { "","","" };
+string theWord;
+string theHint;
 
 int main()
 {
+	enum fields { WORD, HINT, NUM_FIELDS };
+	const int NUM_WORDS = 10;
+	const string WORDS[NUM_WORDS][NUM_FIELDS] =
+	{
+		{"table","I'll put the food on the what?"},
+		{"phone","Call me."},
+		{"strength","Body builders have a great amount of what?"},
+		{"persistent","Keep at it"},
+		{"jumble","It's what the game is all about"},
+		{"writer","an author is also a what?"},
+		{"discount","50% off is a huge what?"},
+		{"highway"," 'I'm on a [blank] to hell!' "},
+		{"single","Hey ladies, he's [blank]"},
+		{"mastermind","The joker is an evil [blank]"}
+	};
+	const string WORDSWithoutHints[NUM_WORDS][NUM_FIELDS] =
+	{
+		{"table","Sorry, you said no hints!"},
+		{"phone","Sorry, you said no hints!"},
+		{"strength","Sorry, you said no hints!"},
+		{"persistent","Sorry, you said no hints!"},
+		{"jumble","Sorry, you said no hints!"},
+		{"writer","Sorry, you said no hints!"},
+		{"discount","Sorry, you said no hints!"},
+		{"highway","Sorry, you said no hints!"},
+		{"single","Sorry, you said no hints!"},
+		{"mastermind","Sorry, you said no hints!"}
+	};
+
+	cout << "\t\t\tWelcome to Word Jumble!\n\n";
+	cout << "Unscramble the letters to make a word.\n";
+	cout << "Enter 'hint' for a hint.\n";
+	cout << "Enter 'quit' to quit the game.\n\n";
+	cout << "Do you want to be able to use hints? 'Yes' or 'No'?\n";
+	cin >> difficultyAnswer;
+
 	while (playAgain == true) {
-		enum fields { WORD, HINT, NUM_FIELDS };
-		const int NUM_WORDS = 5;
-		const string WORDS[NUM_WORDS][NUM_FIELDS] =
-		{
-			{"table","I'll put the food on the what?"},
-			{"phone","Call me."},
-			{"strength","Body builders have a great amount of what?"},
-			{"persistent","Keep at it"},
-			{"jumble","It's what the game is all about"}
-		};
 		for (int i = 0; i < 3; i++)
 		{
 			wordCleared = false;
 			srand(static_cast<unsigned int>(time(0)));
 			int choice = (rand() % NUM_WORDS);
-			string theWord = WORDS[choice][WORD]; // word to guess
-			string theHint = WORDS[choice][HINT]; // hint for word
+			if (difficultyAnswer == "No"||difficultyAnswer=="no")
+			{
+				 theWord = WORDSWithoutHints[choice][WORD]; // word to guess
+				 theHint = WORDSWithoutHints[choice][HINT]; // hint for word
+			}
+			else
+			{
+				 theWord = WORDS[choice][WORD]; // word to guess
+				 theHint = WORDS[choice][HINT]; // hint for word
+			}
 
 			//prevents it from using the same world as last time
 			//checks if the word as used last time
-			while (wordCleared==false)
+			while (wordCleared == false)
 			{
 				if (theWord == lastChoice[0] || theWord == lastChoice[1] || theWord == lastChoice[2])
 				{
@@ -44,7 +81,7 @@ int main()
 					wordCleared = true;
 				}
 			}
-			
+
 			string jumble = theWord; //jumbled version of word
 			int length = jumble.size();
 			for (int i = 0; i < length; i++)
@@ -55,11 +92,6 @@ int main()
 				jumble[index1] = jumble[index2];
 				jumble[index2] = temp;
 			}
-			cout << "\t\t\tWelcome to Word Jumble!\n\n";
-			cout << "Unscramble the letters to make a word.\n";
-			cout << "Enter 'hint' for a hint.\n";
-			cout << "Enter 'quit' to quit the game.\n\n";
-
 
 			cout << "The Jumble is: " << jumble;
 
@@ -86,17 +118,17 @@ int main()
 			{
 				cout << "\nThat's it! You guessed it!\n";
 			}
-			if (guess == "quit") 
+			if (guess == "quit")
 			{
 				return 0;
 			}
 			lastChoice[i] = theWord;
 		}
-		
+
 		cout << "\nWanna play again? 'Yes' or 'No'?\n";
 		cin >> answer;
-		
-		if (answer == "Yes" || answer == "yes") 
+
+		if (answer == "Yes" || answer == "yes")
 		{
 			playAgain = true;
 		}
